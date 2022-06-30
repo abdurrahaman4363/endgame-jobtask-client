f5db6821d41ad19aceccece0cef207832df0efad
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-07-01 00:07:15 +0600

    create a calendar

--- src/Pages/Page/Calendar.js
+++ src/Pages/Page/Calendar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
+import calendar from '../../images/calendar.jfif'
 
 const Calendar = () => {
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl">
-            <div class="card-body">
-                <div> <DayPicker></DayPicker></div>
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row">
+                <img src={calendar} class="max-w-sm rounded-lg shadow-2xl" alt='' />
+                <div>
+                    <DayPicker></DayPicker>
+                </div>
             </div>
         </div>
     );
